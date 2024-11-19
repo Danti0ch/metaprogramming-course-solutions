f3384d44ed74a74a89de61fc225e0e254ab3e00d
Ivanov Mikhail <ivanov.mikhail@phystech.edu>
2024-11-19 23:58:23 +0300

    done

--- span/Span.hpp
+++ span/Span.hpp
@@ -2,13 +2,32 @@
 #include <concepts>
 #include <cstdlib>
 #include <iterator>
+#include <vector>
 
+template <std::size_t extent>
+class AbstractSpan {
+public:
+  AbstractSpan() {}
+  AbstractSpan(size_t size) { assert(size == extent); }
+
+  constexpr size_t Size() const { return extent; }
+};
+
+template <>
+class AbstractSpan <std::dynamic_extent> {
+public:
+  AbstractSpan(size_t size) : size_(size) {}
+
+  constexpr size_t Size() const { return size_; }
+private:
+  size_t size_;
+};
 
 template
   < class T
   , std::size_t extent = std::dynamic_extent
   >
-class Span {
+class Span : public AbstractSpan<extent> {
 public:
   // Reimplement the standard span interface here
   // (some more exotic methods are not checked by the tests and can be sipped)
@@ -16,7 +35,94 @@ public:
   // E.g. instead of subspan, do Subspan.
   // Note that this does not apply to iterator methods like begin/end/etc.
 
+  using iterator = T*;
+  using reverse_iterator = std::reverse_iterator<iterator>;
+  using value_type = std::remove_cv_t<T>;
+  using element_type = T;
+  using size_type = std::size_t;
+  using pointer = iterator;
+  using const_pointer = const T*;
+  using reference = T&;
+  using const_reference = const T&;
+  using difference_type = std::ptrdiff_t;
+
+  T* begin()  const { return data_; }
+  T* end()    const { return data_ + AbstractSpan<extent>::Size() - 1; }
+
+  reference Front() const { return *begin(); }
+  reference Back()  const { return *end(); }
+
+  reverse_iterator rbegin() { return reverse_iterator(end()); }
+  reverse_iterator rend()   { return reverse_iterator(begin()); }
+
+  reference operator[] (size_t i) const {
+    assert(i < AbstractSpan<extent>::Size());
+    return *(data_ + i);
+  }
+
+  constexpr Span() noexcept requires (extent == 0 || extent == std::dynamic_extent) = default;
+
+  constexpr Span(const Span& other) noexcept = default;
+  Span& operator=(const Span& other) noexcept = default;
+
+  template< class It >
+  explicit(extent != std::dynamic_extent)
+  constexpr Span( It first, size_t count ) :
+    AbstractSpan<extent> (count),
+    data_ (std::to_address(first)) {}
+
+  // template< class It, class End >
+  // explicit(extent != std::dynamic_extent)
+  // constexpr Span( It first, End last ) :
+  //   data_(std::to_address(first)),
+  //   AbstractSpan<> (last - first) {}
+
+  template< std::size_t N >
+  constexpr Span( std::type_identity_t<element_type> (&arr)[N] ) noexcept :
+    AbstractSpan<extent> (N),
+    data_(arr) {}
+
+  template< class U, std::size_t N >
+  constexpr Span( std::array<U, N>& arr ) noexcept :
+    AbstractSpan<extent> (N),
+    data_(arr.data()) {}
+
+  template< class U, std::size_t N >
+  constexpr Span( const std::array<U, N>& arr ) noexcept :
+    AbstractSpan<extent> (N),
+    data_(arr.data()) {}
+
+  template< class R >
+  explicit(extent != std::dynamic_extent)
+  constexpr Span( R&& range ) :
+    AbstractSpan<extent> (range.size()),
+    data_(std::data(range)) {}
+
+  ~Span() = default;
+
+  constexpr Span<element_type, std::dynamic_extent> First( size_type Count ) const {
+    return {data_, Count};
+  }
+  template< std::size_t Count >
+  constexpr Span<element_type, Count> First() const {
+    return Span<element_type, Count>{data_, Count};
+  }
+
+  constexpr Span<element_type, std::dynamic_extent> Last( size_type Count ) const {
+    return {end(), Count};
+  }
+  template< std::size_t Count >
+  constexpr Span<element_type, Count> Last() const {
+    return Span<element_type, Count>{end(), Count};
+  }
+
+  constexpr T* Data() const { return data_; }
 private:
   T* data_;
-  // std::size_t extent_; ?
 };
+
+template <typename R>
+Span(R&&) -> Span<std::remove_reference_t<std::ranges::range_reference_t<R>>>;
+
+template <class It>
+Span(It, std::size_t) ->  Span<std::remove_reference_t<std::iter_reference_t<It>>>;
--- spy/Spy.hpp
+++ spy/Spy.hpp
@@ -1,18 +1,144 @@
 #pragma once
 #include <concepts>
+#include <utility>
 
+template <typename T>
+class Spy;
+
+template <class T>
+class SpyProxy {
+public:
+  SpyProxy(Spy<T> *spy):
+    spy_(spy)
+    { spy_->AddProxy(); }
+
+  T *operator -> () {
+    return &spy_->value_;
+  }
+
+  ~SpyProxy() {
+    spy_->RemoveProxy();
+  }
+
+private:
+  Spy<T> *spy_;
+};
+
+class LoggerBaseAbstract {
+public:
+  virtual ~LoggerBaseAbstract() = default;
+
+  virtual void Call(unsigned int val) = 0;
+  virtual LoggerBaseAbstract* Clone() = 0;
+};
+
+template<std::invocable<unsigned int> Logger>
+class LoggerBase {
+};
+
+template<std::invocable<unsigned int> Logger>
+requires std::copyable<Logger>
+class LoggerBase<Logger> : public LoggerBaseAbstract {
+public:
+  LoggerBase(Logger&& logger) :
+    logger_(std::move(logger))
+    {}
+
+  LoggerBase(Logger& logger) :
+    logger_((logger))
+    {}
+
+  ~LoggerBase() override = default;
+
+  void Call(unsigned int val) override
+  { logger_(val); }
+
+  LoggerBaseAbstract* Clone() override {
+    if constexpr(std::is_copy_constructible_v<Logger>) {
+      return new LoggerBase(logger_);
+    }
+  }
+
+private:
+  Logger logger_;
+};
+
+template<std::invocable<unsigned int> Logger>
+requires std::movable<Logger>
+class LoggerBase<Logger> : public LoggerBaseAbstract {
+public:
+  LoggerBase(Logger&& logger) :
+    logger_(std::move(logger))
+    {}
+
+  ~LoggerBase() override = default;
+
+  void Call(unsigned int val) override
+  { logger_(val); }
+
+  LoggerBaseAbstract* Clone() override {
+    return nullptr;
+  }
+
+private:
+  Logger logger_;
+};
 
 // Allocator is only used in bonus SBO tests,
 // ignore if you don't need bonus points.
 template <class T /*, class Allocator = std::allocator<std::byte>*/ >
 class Spy {
+private:
+  friend SpyProxy<T>;
+  void AddProxy() {
+    proxies_++;
+    if (logger_) {
+      expr_calls_++;
+    }
+  }
+
+  void RemoveProxy() {
+    proxies_--;
+    if (proxies_ == 0 && logger_) {
+      logger_->Call(expr_calls_);
+      expr_calls_ = 0;
+    }
+  }
+
 public:
-  explicit Spy(T /* , const Allocator& alloc = Allocator()*/ );
+  explicit Spy(T &value/* , const Allocator& alloc = Allocator()*/ ) 
+    requires std::copyable<T> :
+    value_(value),
+    logger_(nullptr),
+    expr_calls_(0),
+    proxies_(0)
+    {};
 
-  T& operator *();
-  const T& operator *() const;
+  explicit Spy(T &&value/* , const Allocator& alloc = Allocator()*/ ) 
+    requires std::movable<T> :
+    value_(std::move(value)),
+    expr_calls_(0),
+    proxies_(0),
+    logger_(nullptr)
+    {};
 
-  /* unspecified */ operator ->();
+  explicit Spy(/* , const Allocator& alloc = Allocator()*/ ) 
+    requires std::default_initializable<T> :
+    expr_calls_(0),
+    proxies_(0)
+    {logger_ = nullptr; };
+
+  ~Spy() {
+    if (logger_)
+      delete logger_;
+  }
+  T& operator *() { return value_; }
+  const T& operator *() const { return value_; }
+
+  /* https://stackoverflow.com/questions/8777845/overloading-member-access-operators */
+  SpyProxy<T> operator ->() {
+    return SpyProxy(this);
+  }
 
   /*
    * if needed (see task readme):
@@ -24,13 +150,60 @@ public:
   */
 
   // Resets logger
-  void setLogger();
+  void setLogger() {
+    if (logger_)
+      delete logger_;
+    logger_ = nullptr;
+  }
 
-  template <std::invocable<unsigned int> Logger> requires /* see task readme */
-  void setLogger(Logger&& logger);
+  template <std::invocable<unsigned int> Logger> 
+  requires (
+    (!std::movable<T>     || std::move_constructible<Logger>) &&
+    (!std::copyable<T>    || std::copy_constructible<Logger>)
+  )
+  void setLogger(Logger&& logger) {
+    setLogger();
+    logger_ = new LoggerBase<std::remove_reference_t<Logger>>(std::forward<Logger>(logger));
+  }
+
+  bool operator==(const Spy& other) const
+    requires std::equality_comparable<T>
+  { return value_ == other.value_; }
+
+  Spy& operator=(const Spy& other)
+    requires(std::copyable<T>) {
+    Spy tmp = other;
+    *this = std::move(tmp);
+    return *this;
+  }
+
+  Spy& operator=(Spy&& other)
+    requires(std::movable<T>) {
+    std::swap(this->value_, other.value_);
+    std::swap(this->expr_calls_, other.expr_calls_);
+    std::swap(this->proxies_, other.proxies_);
+    std::swap(this->logger_, other.logger_);
+    return *this;
+  }
+
+  Spy(const Spy& other) requires(std::copy_constructible<T>)
+    : value_(other.value_) {
+    if (other.logger_) {
+      setLogger();
+      logger_ = other.logger_->Clone();
+    }
+  }
+
+  Spy(Spy&& other)
+    requires std::move_constructible<T> :
+    value_(std::move(other.value_))
+  { std::swap(logger_, other.logger_); }
 
 private:
   T value_;
+  int expr_calls_;
+  int proxies_;
+
   // Allocator allocator_;
-  // Logger logger_; ???
+  LoggerBaseAbstract *logger_;
 };
