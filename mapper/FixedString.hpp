427b18286d9ea835b8cfcbd5ddb1fcb14c7c7c08
Ivanov Mikhail <ivanov.mikhail@phystech.edu>
2024-12-11 01:34:48 +0300

    done

--- mapper/FixedString.hpp
+++ mapper/FixedString.hpp
@@ -1,12 +1,24 @@
 #pragma once
 
+#include <string_view>
 
-template<size_t max_length>
+template <size_t max_length>
 struct FixedString {
-  FixedString(const char* string, size_t length);
-  operator std::string_view() const;
+  constexpr FixedString(const char *string, const size_t length) :
+    length(length) {
+    std::copy(string, string + length, impl);
+    if (length < max_length)
+      impl[length] = 0;
+  }
 
-  // std::string impl; ???
+  constexpr operator std::string_view() const {
+    return {impl, length};
+  }
+
+  char impl[max_length]{0};
+  size_t length;
 };
 
-// operator ""_cstr ?
+constexpr FixedString<256> operator""_cstr(const char *string, size_t length) {
+  return {string, length};
+}
--- mapper/PolymorphicMapper.hpp
+++ mapper/PolymorphicMapper.hpp
@@ -1,12 +1,32 @@
-#pragma once
-
 #include <optional>
+#include <type_traits>
+#include <iostream>
 
+template <typename From, auto targetValue>
+struct Mapping
+{
+  using TargetType = decltype(targetValue);
+};
 
-template <class From, auto target>
-struct Mapping;
+template <typename Base, typename Target, typename... Mappings>
+ requires(std::same_as<
+               std::remove_cv_t<typename Mappings::TargetType>,
+               Target> &&
+           ...)
+struct PolymorphicMapper
+{
+  static std::optional<Target> map(const Base&)
+  { return std::nullopt; }
+};
 
-template <class Base, class Target, class... Mappings>
-struct PolymorphicMapper {
-  static std::optional<Target> map(const Base& object);
+template <typename Base, typename Target, auto targetValue, typename Cast, typename... Mappings>
+requires std::is_base_of_v<Base, Cast>
+struct PolymorphicMapper<Base, Target, Mapping<Cast, targetValue>, Mappings...>
+{
+  static std::optional<Target> map(const Base& object)
+  {
+    if (const Cast* castObject = dynamic_cast<const Cast*>(&object))
+      return std::optional<Target>{targetValue};
+    return PolymorphicMapper<Base, Target, Mappings...>::map(object);
+  }
 };
